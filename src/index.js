2ce83cf2c96d243498823169a15ef9aefb0f0d01
qige2016 <286882998@qq.com>
2020-05-14 19:34:54 +0800

    :art: update axios.get in handleSubject

--- src/index.js
+++ src/index.js
@@ -5,22 +5,21 @@ const aes = require('../utils/aes')
 const data = require('../config')
 
 const url = 'https://wangda.chinamobile.com/'
+const subjectId = 'd6bd78b1-911d-4017-aa11-0c054cb06f72'
 
 class AutoWangda {
   /**
    *Creates an instance of AutoWangda.
    * @param data {Object} 登录参数
    */
-  constructor({ data, url }) {
+  constructor({ data, url, subjectId }) {
     this.data = data
     this.url = url
+    this.subjectId = subjectId
   }
   async init() {
     const authorization = await this.handleLogin()
-    const list = await this.handleSubject(
-      'd6bd78b1-911d-4017-aa11-0c054cb06f72',
-      authorization
-    )
+    const list = await this.handleSubject(authorization)
   }
   async handleLogin() {
     const res = await this.requestMembersEncrypt(
@@ -52,44 +51,44 @@ class AutoWangda {
       }
     })
   }
-  async handleSubject(subjectId = '', authorization) {
-    const res = await this.requestSubject(subjectId, authorization)
-    return res
+  async handleSubject(authorization) {
+    const res = await this.requestRegister(authorization)
+    const response = await this.requestChapter(
+      res.data.versionId,
+      authorization
+    )
+    return response.data
   }
-  requestSubject(subjectId, authorization) {
-    axios
-      .post(
-        url + 'api/v1/course-study/course-front/register',
-        qs.stringify({ courseId: subjectId }),
-        {
-          headers: {
-            Authorization: authorization,
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-          }
+  requestRegister(authorization) {
+    return axios.post(
+      url + 'api/v1/course-study/course-front/register',
+      qs.stringify({ courseId: this.subjectId }),
+      {
+        headers: {
+          Authorization: authorization,
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
         }
-      )
-      .then(res => {
-        const versionId = res.data.versionId
-        return axios.get(
-          url + 'api/v1/course-study/course-front/chapter-progress',
-          {
-            params: {
-              courseId: subjectId,
-              versionId: versionId,
-              isRegister: false
-            }
-          },
-          {
-            headers: {
-              Authorization: authorization,
-              'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-            }
-          }
-        )
-      })
+      }
+    )
+  }
+  requestChapter(versionId, authorization) {
+    return axios.get(
+      url + 'api/v1/course-study/course-front/chapter-progress',
+      {
+        params: {
+          courseId: this.subjectId,
+          versionId: versionId,
+          isRegister: false
+        },
+        headers: {
+          Authorization: authorization,
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+        }
+      }
+    )
   }
   async handleCourse() {}
   requestCourse() {}
@@ -97,5 +96,5 @@ class AutoWangda {
   requestVideoProgress() {}
 }
 
-const autoWangda = new AutoWangda({ data, url })
+const autoWangda = new AutoWangda({ data, url, subjectId })
 autoWangda.init()
