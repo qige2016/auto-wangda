ffbe6d1d91f421049681ae7c4b2d0ec55a3a8b12
qige2016 <286882998@qq.com>
2020-05-17 00:38:55 +0800

    :rocket: update .babelrc

---
+++ .bili.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  input: [
+    'src/AutoWangda.js',
+    'src/handleCourse.js',
+    'src/handlePostAll.js',
+    'src/handleSubject.js',
+    'src/index.js',
+    'src/login.js',
+    'src/Schedule.js'
+  ]
+}
