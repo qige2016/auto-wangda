651771224a42bb7ee887449cfb8248774c645652
qige2016 <286882998@qq.com>
2020-05-17 02:40:17 +0800

    :nut_and_bolt: move structure

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-require('../lib')
+const inquirer = require('inquirer')
+const config = require('../config')
+const loginForm = require('../config/loginForm')
+const AutoWangda = require('../lib')
+
+const promptList = [
+  {
+    type: 'input',
+    message: '请输入手机号码/员工编号:',
+    name: 'username'
+  },
+  {
+    type: 'password',
+    message: '请输入登录密码:',
+    name: 'password'
+  },
+  {
+    type: 'input',
+    message: '请输入专题ID:',
+    name: 'subjectId'
+  }
+]
+inquirer.prompt(promptList).then(({ username, password, subjectId }) => {
+  const data = Object.assign({ username, password }, loginForm)
+  const autoWangda = new AutoWangda({ data, subjectId })
+  autoWangda.run()
+})
