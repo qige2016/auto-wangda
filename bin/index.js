cc8ebfbc194dd9e4857c3247a39df3fc636181d4
qige2016 <286882998@qq.com>
2020-05-22 18:47:42 +0800

    :star2: add time and finish pattern

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const inquirer = require('inquirer')
 const loginForm = require('../config/loginForm')
 const AutoWangda = require('../lib')
+const store = require('../utils/store')
 
 const promptList = [
   {
@@ -19,10 +20,28 @@ const promptList = [
     type: 'input',
     message: '请输入专题ID:',
     name: 'subjectId'
+  },
+  {
+    type: 'list',
+    message: '请选择一种方式:',
+    name: 'pattern',
+    choices: [
+      {
+        name: '计时: 默认1分钟请求1次，计时生效还需从专题点击进入课程',
+        value: 'time'
+      },
+      {
+        name: '完成: 默认即刻完成',
+        value: 'finish'
+      }
+    ]
   }
 ]
-inquirer.prompt(promptList).then(({ username, password, subjectId }) => {
-  const data = Object.assign({ username, password }, loginForm)
-  const autoWangda = new AutoWangda({ data, subjectId })
-  autoWangda.run()
-})
+inquirer
+  .prompt(promptList)
+  .then(({ username, password, subjectId, pattern }) => {
+    store.set('pattern', pattern)
+    const data = Object.assign({ username, password }, loginForm)
+    const autoWangda = new AutoWangda({ data, subjectId })
+    autoWangda.run()
+  })
