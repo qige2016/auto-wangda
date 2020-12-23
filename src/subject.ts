b7a8b3653d91ae762fa4304b055904d9ce46dc2b
qige2016 <286882998@qq.com>
2020-12-23 17:11:47 +0800

    feat: add subject

---
+++ src/subject.ts
@@ -0,0 +1,27 @@
+import { get, post } from './http'
+
+const registerUrl = 'api/v1/course-study/course-front/register'
+
+const chapterProgressUrl = 'api/v1/course-study/course-front/chapter-progress'
+
+export const subject = async (courseId: string): Promise<string[]> => {
+  const {
+    data: { versionId }
+  } = await post(registerUrl, { courseId })
+
+  const { data } = await get(chapterProgressUrl, {
+    courseId,
+    versionId,
+    isRegister: false
+  })
+
+  const arr: string[] = []
+
+  for (const { courseChapterSections } of data) {
+    for (const { sectionType, resourceId } of courseChapterSections) {
+      sectionType === 10 && arr.push(resourceId)
+    }
+  }
+
+  return arr
+}
