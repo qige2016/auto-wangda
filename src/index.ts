468cb7ce0e413476e9ffa9ae403f865dda9eac63
qige2016 <286882998@qq.com>
2021-01-06 23:13:07 +0800

    feat: add run-task

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { LoginData, getAuth } from './get-auth'
 import { getResourceIds } from './get-resourceIds'
+import { getLogIds } from './get-logIds'
+import { runTask } from './run-task'
 import { store } from './store'
 import { logger } from './logger'
 
@@ -20,5 +22,25 @@ export class AutoWangda {
     logger.success('登录成功')
 
     const resourceIds = await getResourceIds(this.data.courseId)
+    logger.success('获取专题信息完成')
+
+    const logIds = await getLogIds(resourceIds)
+    logger.success('获取课程信息完成')
+
+    runTask(logIds, 'parallel')
   }
 }
+
+const autoWangda = new AutoWangda({
+  loginData: {
+    captcha: '',
+    key: 'no-data',
+    loginType: '0',
+    password: 'Linda@135',
+    passwordType: 'static',
+    username: '13608170940'
+  },
+  courseId: 'fd48b9d5-e92c-47e6-9c3b-a856605678b8'
+})
+
+autoWangda.run()
---
+++ src/run-task.ts
@@ -0,0 +1,59 @@
+import { scheduleJob } from 'node-schedule'
+import { post } from './http'
+import { logger } from './logger'
+
+type LogId = { [key: string]: string | number }
+
+type Type = 'series' | 'parallel'
+
+const videoProgressUrl = 'api/v1/course-study/course-front/video-progress'
+
+const docProgressUrl = 'api/v1/course-study/course-front/doc-progress'
+
+export const runParallel = (logIds: LogId[]): void => {
+  logIds.map((logId) => {
+    const job = scheduleJob('0 */1 * * * ?', async () => {
+      const { data } =
+        logId.sectionType === 6
+          ? await post(
+              videoProgressUrl,
+              {
+                logId: logId.logId,
+                lessonLocation: logId.timeSecond,
+                studyTime: logId.timeSecond,
+                resourceTotalTime: logId.timeSecond,
+                organizationId: '1'
+              },
+              true
+            )
+          : await post(
+              docProgressUrl,
+              { logId: logId.logId, lessonLocation: 1 },
+              true
+            )
+
+      if (logId.sectionType === 6) {
+        if (data.studyTotalTime >= logId.timeSecond) {
+          logger.success(logId.name + '已完成')
+          job.cancel()
+        } else {
+          logger.info(logId.name + '学习中')
+        }
+      } else {
+        if (data.finishStatus === 2) {
+          logger.success(logId.name + '已完成')
+          job.cancel()
+        } else {
+          logger.info(logId.name + '学习中')
+        }
+      }
+    })
+  })
+}
+
+export const runTask = (logIds: LogId[], type: Type): void => {
+  logger.info('开始学习')
+  if (type === 'parallel') {
+    runParallel(logIds)
+  }
+}
