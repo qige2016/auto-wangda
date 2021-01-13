64fe1131aa1ea4093c59b8ace49a1815ea363fd5
qige2016 <286882998@qq.com>
2021-01-14 00:40:33 +0800

    chore: debug

--- src/run-task.ts
+++ src/run-task.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestConfig } from 'axios'
 import ProgressBar from 'progress'
 import { scheduleJob } from 'node-schedule'
 import { logger } from './logger'
@@ -11,55 +12,78 @@ const videoProgressUrl = 'api/v1/course-study/course-front/video-progress'
 
 const docProgressUrl = 'api/v1/course-study/course-front/doc-progress'
 
-export const runParallel = (logIds: LogId[]): void => {
-  const cachedResponses: any[] = []
-  const job = scheduleJob('0 */1 * * * ?', async () => {
-    console.log(cachedResponses.length)
-    cachedResponses.length === 0 && job.cancel()
-    const responses = await fetchParallel(
-      logIds
-        .filter((logId, i) =>
-          logId.sectionType === 6
-            ? cachedResponses[i] &&
-              cachedResponses[i]?.studyTotalTime < logId.timeSecond
-            : cachedResponses[i] && cachedResponses[i]?.finishStatus !== 2
-        )
-        .map((logId) =>
-          logId.sectionType === 6
-            ? {
-                method: 'POST' as const,
-                url: videoProgressUrl,
-                data: {
-                  logId: logId.logId,
-                  lessonLocation: logId.timeSecond,
-                  studyTime: logId.timeSecond,
-                  resourceTotalTime: logId.timeSecond,
-                  organizationId: '1'
-                }
+export const runParallel = async (logIds: LogId[]): Promise<void> => {
+  const cachedResponses = await fetchParallel(
+    logIds.map(
+      (logId) =>
+        logId.sectionType === 6
+          ? {
+              method: 'POST',
+              url: videoProgressUrl,
+              data: {
+                logId: logId.logId,
+                lessonLocation: logId.timeSecond,
+                studyTime: logId.timeSecond,
+                resourceTotalTime: logId.timeSecond,
+                organizationId: '1'
               }
-            : {
-                method: 'POST' as const,
-                url: docProgressUrl,
-                data: {
-                  logId: logId.logId,
-                  lessonLocation: 1
-                }
+            }
+          : {
+              method: 'POST',
+              url: docProgressUrl,
+              data: {
+                logId: logId.logId,
+                lessonLocation: 1
               }
-        ),
+            },
       30
     )
+  )
+
+  const job = scheduleJob('0 */1 * * * ?', async () => {
+    cachedResponses.length === 0 && job.cancel()
+    const requests: AxiosRequestConfig[] = []
+    for (let i = 0; i < logIds.length; i++) {
+      const logId = logIds[i]
+      const { data } = cachedResponses[i]
+      if (logId.sectionType === 6) {
+        data?.studyTotalTime < logId.timeSecond &&
+          (requests[i] = {
+            method: 'POST',
+            url: videoProgressUrl,
+            data: {
+              logId: logId.logId,
+              lessonLocation: logId.timeSecond,
+              studyTime: logId.timeSecond,
+              resourceTotalTime: logId.timeSecond,
+              organizationId: '1'
+            }
+          })
+      } else {
+        data?.finishStatus !== 2 &&
+          (requests[i] = {
+            method: 'POST',
+            url: docProgressUrl,
+            data: {
+              logId: logId.logId,
+              lessonLocation: 1
+            }
+          })
+      }
+    }
+    const responses = await fetchParallel(requests, 30)
     for (let i = 0; i < logIds.length; i++) {
       const logId = logIds[i]
       const { data } = responses[i]
-      cachedResponses.push(data)
+      cachedResponses[i] = data
       const bar = new ProgressBar(`${logId.name} :percent`, {
         total: logId.timeSecond as number
       })
       logId.sectionType === 6
         ? bar.complete
           ? bar.terminate()
-          : bar.tick(data.studyTotalTime)
-        : data.finishStatus === 2
+          : bar.tick(data?.studyTotalTime)
+        : data?.finishStatus === 2
         ? console.log(`${logId.name} complete`)
         : console.log(`${logId.name} ing`)
     }
